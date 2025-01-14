4b7551cd53fffc58735b52d478a115b201da0b0e
KhanAbdullah <khanabdulla7862@gmail.com>
2025-01-14 14:47:46 +0530

    Jan-14-25

---
+++ productManagemenSystem/productManagemenSystem/src/test/java/com/chetu/productManagementSystem/ProductManagemenSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chetu.productManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagemenSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
