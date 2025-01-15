a81af2492fa8f0a9cfdf92b7b7b3d10d71612a1c
KhanAbdullah <khanabdulla7862@gmail.com>
2025-01-15 11:28:38 +0530

    Jan-15-25

--- productManagemenSystem/productManagemenSystem/src/main/java/com/chetu/productManagementSystem/repositories/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.chetu.productManagementSystem.repositories;
-
-import com.chetu.productManagementSystem.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,Integer> {
-}
